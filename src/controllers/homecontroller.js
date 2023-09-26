1fc1d6e4f44552a3766e21f3f0432c093ed1c321
dctamchi <dctamchi@gmail.com>
2023-09-26 20:09:55 +0700

    3snd commit

--- src/controllers/homecontroller.js
+++ src/controllers/homecontroller.js
@@ -1,8 +1,16 @@
 
+import db from '../models/index';
 
+let getHomePage = async(req, res) => {
+    try {
+        let data = await db.User.findAll();
+        
+        return res.render('homepage.ejs',{data: JSON.stringify(data)});
+    } catch (e){
+        console.log(e)
+    } 
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs')
+   
 }
 let getAboutPage = (req,res) =>{
     return res.render('test/about.ejs')
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectionDB";
+
 require('dotenv').config();
 
 let app = express();
@@ -11,6 +13,9 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB();
+
 
 let port = process.env.PORT || 6969;
 app.listen(port , () => {
